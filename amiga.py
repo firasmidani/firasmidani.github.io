20a3b4a78e7b73533494565d45e124b16a9a5161
Firas Midani <firasmidani@gmail.com>
2020-03-23 13:41:08 -0500

    Create amiga.py

---
+++ amiga.py
@@ -0,0 +1,21 @@
+---
+layout: software
+title: "AMiGA: Analysis of Microbial Growth Assays"
+date: 2020-03-20
+use_math: true
+comments: False
+---
+
+# AMIGA: **A**nalysis of **Mi**crobial **G**rowth **A**ssays
+
+
+
+```zsh
+python amiga.py -i /Uers/firasmidani/Downloads/test/
+```
+
+Duke Today. <a href="https://today.duke.edu/2018/05/ai-detects-patterns-gut-microbes-cholera-risk">AI Detects Patterns of Gut Microbes for Cholera Risk</a>. \[May 7, 2018] by Marla Vacek Broadfoot. 
+
+
+<br><br><br>
+![footer_banner](/assets/img/mosaic_footer.png)
